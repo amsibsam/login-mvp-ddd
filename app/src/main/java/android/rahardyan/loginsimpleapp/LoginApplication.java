552cf2e3f6a7a99f508e36a33f4261fe8b521a50
amsibsam <amsibsam@gmail.com>
2017-08-09 22:44:40 +0700

    add application class

---
+++ app/src/main/java/android/rahardyan/loginsimpleapp/LoginApplication.java
@@ -0,0 +1,14 @@
+package android.rahardyan.loginsimpleapp;
+
+import android.app.Application;
+
+/**
+ * Created by rahardyan on 09/08/17.
+ */
+
+public class LoginApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
